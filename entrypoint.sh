28e9a290c2778d77b0268a7343befa6638963995
ledsusop <ledsusop@gmail.com>
2018-05-27 20:15:20 -0700

    add echo of path

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ tar -xzvf ${tarloc}/apache-maven-3.5.3-bin.tar.gz
 export M2_HOME=/opt/apache-maven-3.5.3
 export PATH=$PATH:$M2_HOME/bin
 
+echo "CURRENT PATH ${PATH}"
+echo "WHICH JAVA ${JAVA_HOME}"
+
 cd /root
 
 rm -rf "/root/${PROJECT_NAME}-webapp"
